e6fddf39a6cfdbde9c215c611b24b5d2a4a3f5c5
s.budnikov <sergei.budnikov@smartseeds.ru>
2024-06-04 10:51:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ApiClient;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+var routeServiceRequest = new RouteServiceRequest("345345", DateTimeOffset.Parse("30"));
+
+var message = CreateMessage(routeServiceRequest);
+try
+{
+    using var httpClient = HttpClientFactory.Create();
+    var response = await httpClient.SendAsync(message);
+    var content = await response.Content.ReadAsStringAsync();
+    var responseObject = content.Json<RouteServiceResponse>();
+    return (int)responseObject.RouteId;
+}
+catch (InvalidCastException castException)
+{
+    //_logger.LogError(castException, "Route service returned invalid data");
+    throw;
+}
+catch (Exception e)
+{
+    //_logger.LogError(e, "Route service return exception");
+    throw;
+}
+
+HttpRequestMessage CreateMessage(RouteServiceRequest routeServiceRequest)
+{
+    var queryParams = new Dictionary<string, string>
+            {
+                {"waypoints", routeServiceRequest.Waypoints},
+                {"departuretime", routeServiceRequest.DepartureTime.ToString()},
+                {"answermode", routeServiceRequest.AnswerMode},
+            };
+
+    var url = QueryHelpers.AddQueryString(@$"/cargorouter/route", queryParams);
+
+    var message = new HttpRequestMessage(HttpMethod.Get, url);
+
+    message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "_tokenStorage.Token");
+
+    return message;
+}
---
+++ RouteServiceRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient;
+
+public class RouteServiceRequest
+{
+    public string Waypoints { get; }
+    public DateTimeOffset DepartureTime { get; }
+
+    public bool UseTolls { get; set; }
+    public bool IgnoreJams { get; set; }
+    public string AnswerMode { get; set; } = "Route";
+
+    public RouteServiceRequest(string waypoints, DateTimeOffset departureTime)
+    {
+        Waypoints = waypoints;
+        DepartureTime = departureTime;
+    }
+}
